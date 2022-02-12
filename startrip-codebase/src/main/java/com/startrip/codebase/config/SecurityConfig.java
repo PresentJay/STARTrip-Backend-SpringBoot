60ab00883a81590c50edc3f8694c3ff8e8d8a80c
Hoon9901 <shonn.dev@gmail.com>
2022-02-12 18:18:20 +0900

    chore: Security Config 파일 수정

--- startrip-codebase/src/main/java/com/startrip/codebase/config/SecurityConfig.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/config/SecurityConfig.java
@@ -70,9 +70,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
 
-                .antMatchers("/").permitAll()
-                .antMatchers("/api/user/signup").permitAll()
-                .antMatchers("/h2-console").permitAll();
                 .antMatchers("/api/user", "/api/user/signup", "/api/user/login").permitAll()
 
                 .anyRequest().authenticated()
