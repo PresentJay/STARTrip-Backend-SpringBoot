ec533b20b6c90840f8a94358a213b10e7f97cd8c
Hoon9901 <shonn.dev@gmail.com>
2022-02-16 18:14:55 +0900

    feat: add ping api

---
+++ startrip-codebase/src/main/java/com/startrip/codebase/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.startrip.codebase.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/ping")
+    public String test(){
+        return "pong";
+    }
+}
