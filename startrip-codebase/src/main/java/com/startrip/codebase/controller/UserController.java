2dd42e52c11b1ded9de9154045ae57436dcd441f
min-0 <dhe08133@naver.com>
2022-02-20 20:27:16 +0900

    git branch change

--- startrip-codebase/src/main/java/com/startrip/codebase/controller/UserController.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.startrip.codebase.controller;
 
-import com.nimbusds.oauth2.sdk.TokenResponse;
 import com.startrip.codebase.domain.user.User;
 import com.startrip.codebase.dto.LoginDto;
 import com.startrip.codebase.dto.SignUpDto;
@@ -8,8 +7,13 @@ import com.startrip.codebase.jwt.JwtFilter;
 import com.startrip.codebase.jwt.TokenProvider;
 import com.startrip.codebase.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
