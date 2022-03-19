bdeb2af985dcd2ea85cf6af21de13a408fb0b213
kgyury <mandarining0918@gmail.com>
2022-03-20 02:35:46 +0900

    feat: create Category Controller

---
+++ startrip-codebase/src/main/java/com/startrip/codebase/controller/CategoryController.java
@@ -0,0 +1,13 @@
+package com.startrip.codebase.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/category")
+public class CategoryController {
+
+}
