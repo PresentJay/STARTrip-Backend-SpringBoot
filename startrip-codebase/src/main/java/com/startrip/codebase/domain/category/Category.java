25fae3aa4f70451db669e47d385d173ac65e4fbd
kgyury <mandarining0918@gmail.com>
2022-03-20 20:23:01 +0900

    feat: changing relationship, strong -> weak about categoryParentId

--- startrip-codebase/src/main/java/com/startrip/codebase/domain/category/Category.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/domain/category/Category.java
@@ -1,21 +1,18 @@
 package com.startrip.codebase.domain.category;
 
-import com.startrip.codebase.dto.category.CreateCategoryDto;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.startrip.codebase.domain.category.dto.CreateCategoryDto;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "\"Category\"")
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Category {
 
     @Id
@@ -23,9 +20,8 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId; // PK
 
-    @ManyToOne(fetch = FetchType.EAGER) //상위 카테고리
-    @JoinColumn (name ="category_parent")
-    private Category categoryParent;
+    @Column(name = "category_parent_id")
+    private Long categoryParentID;
 
     @Column(name = "category_name")
     private String categoryName;
@@ -35,17 +31,14 @@ public class Category {
     @JoinColumn (name ="category_child_id")
     private List<Category> categoryChildId = new ArrayList<>();
 
-
     @Column(nullable = false)
     private Integer depth;
 
-
-    /*
-    public Category(Category categoryParent, String categoryName, Integer depth){
-        this.categoryParent = categoryParent;
-        this.categoryName = categoryName;
-        this.depth = depth;
-    } */
-
+    public void update(CreateCategoryDto dto){
+        this.categoryParentID = dto.getCategoryParentId();
+        this.categoryId = dto.getCategoryId();
+        this.categoryName = dto.getCategoryName();
+        this.depth = dto.getDepth();
+    }
 
 }
