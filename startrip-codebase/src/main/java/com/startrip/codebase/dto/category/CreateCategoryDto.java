cb479313422fdd93f1810e2b36bdb09f24fbe45a
kgyury <mandarining0918@gmail.com>
2022-03-20 17:31:23 +0900

    feat: Impelement create builder

--- startrip-codebase/src/main/java/com/startrip/codebase/dto/category/CreateCategoryDto.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/dto/category/CreateCategoryDto.java
@@ -16,20 +16,24 @@ public class CreateCategoryDto {
     private String categoryName;
     private Integer depth;
 
-
     public CreateCategoryDto(Category entity){
         this.categoryId = entity.getCategoryId();
         this.categoryName = entity.getCategoryName();
         this.depth = entity.getDepth();
 
         // 최초의, rootCategory 생성시엔 NullPointer 에러가 날 수 있다.
-        if(entity.getCategoryParentId() == null)
+        if(entity.getCategoryParent() == null)
             this.categoryParentId = 0L; // 최초로, parent가 없는 거라면 0값을 가지도록 했음
         else
-            this.categoryParentId = entity.getCategoryParentId().getCategoryId(); // 부모 id가 있다면 정상적으로 받기
+            this.categoryParentId = entity.getCategoryParent().getCategoryId(); // 부모 id가 있다면 정상적으로 받기
     }
 
-
-
-
+    public Category createCategory(){
+        Category category = Category.builder()
+                .categoryId(categoryId)
+                .categoryName(categoryName)
+                .depth(depth)
+                .build();
+        return category;
+    }
 }
