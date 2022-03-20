4b1fe2d67cff5c952f205ce45cf075f24f1af3da
kgyury <mandarining0918@gmail.com>
2022-03-20 15:58:01 +0900

    feat: Create Method for NullPointerException

---
+++ startrip-codebase/src/main/java/com/startrip/codebase/dto/category/CreateCategoryDto.java
@@ -0,0 +1,35 @@
+package com.startrip.codebase.dto.category;
+
+
+import com.startrip.codebase.domain.category.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateCategoryDto {
+
+    private Long categoryId;
+    private Long categoryParentId;
+    private String categoryName;
+    private Integer depth;
+
+
+    public CreateCategoryDto(Category entity){
+        this.categoryId = entity.getCategoryId();
+        this.categoryName = entity.getCategoryName();
+        this.depth = entity.getDepth();
+
+        // 최초의, rootCategory 생성시엔 NullPointer 에러가 날 수 있다.
+        if(entity.getCategoryParentId() == null)
+            this.categoryParentId = 0L; // 최초로, parent가 없는 거라면 0값을 가지도록 했음
+        else
+            this.categoryParentId = entity.getCategoryParentId().getCategoryId(); // 부모 id가 있다면 정상적으로 받기
+    }
+
+
+
+
+}
