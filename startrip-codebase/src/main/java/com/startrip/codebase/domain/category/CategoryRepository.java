0eb741e2709436b0e604df7e7207f565f9b48ded
kgyury <mandarining0918@gmail.com>
2022-03-20 14:20:00 +0900

    feat: Add category Jpa method

--- startrip-codebase/src/main/java/com/startrip/codebase/domain/category/CategoryRepository.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/domain/category/CategoryRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
-    Optional<Category> findCategoryByDepth(Integer depth);
+    Optional<Category> findCategoryByDepth(Integer depth); // 연결 깊이에 따라 조회..? root로 보면 될 것 같긴 한데
+    Optional<Category> findByCategoryName(String name); //카테고리 이름으로 조회하기
 }
