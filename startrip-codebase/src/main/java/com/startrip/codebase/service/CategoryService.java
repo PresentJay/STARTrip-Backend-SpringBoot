c4311d0b5e53d5df5745a14973aaecb0519e77f2
kgyury <mandarining0918@gmail.com>
2022-03-22 23:18:04 +0900

    feat: Implement createCategory method, Strong Connetion

--- startrip-codebase/src/main/java/com/startrip/codebase/service/CategoryService.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/service/CategoryService.java
@@ -4,11 +4,13 @@ package com.startrip.codebase.service;
 import com.startrip.codebase.domain.category.Category;
 import com.startrip.codebase.domain.category.CategoryRepository;
 import com.startrip.codebase.domain.category.dto.CreateCategoryDto;
+import com.startrip.codebase.domain.category.dto.UpdateCategoryDto;
 import com.startrip.codebase.domain.event.Event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -21,16 +23,44 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    // POST: api/categories
     public void createCategory(CreateCategoryDto dto) {
-        Category category = Category.builder()
-                .categoryParentID(dto.getCategoryParentId()) //create할 땐 parent를 안 넣나?
-                .categoryName(dto.getCategoryName())
-                .depth(dto.getDepth())
-                .build();
-        categoryRepository.save(category);
-        log.info(category.toString());
+
+        //생성
+        Category category = Category.createCategory(dto);
+
+        // 상위 카테고리 넣어주기
+        if(dto.getCategoryParentId() == null){
+            /* 대분류의 생성일 경우 */
+            // 진짜 Root가 없는지 확인하고.
+            Category rootCategory = categoryRepository.findById(0L)
+                .orElseGet( ()-> Category.builder()
+                    .depth(0)
+                    .categoryName("ROOT")
+                    .build()
+                );
+            category.builder()
+                    .categoryParent(rootCategory)
+                    .depth(1)
+                    .build();
+        } else {
+            /* 하위분류의 생성일 경우 */
+
+            // 해당 부모카테고리가 있는지 찾아본다, Optional이므로 없으면 예외발생
+            Long categoryParentId = dto.getCategoryParentId();
+            Category parentCategory = categoryRepository.findById(categoryParentId)
+                    .orElseThrow(() -> new IllegalArgumentException("부모 카테고리 부재"));
+            category.builder()
+                    .categoryParent(parentCategory)
+                    .depth(parentCategory.getDepth() + 1)
+                    .build();
+        }
+
+        categoryRepository.save(category); //저장
+        log.info(category.getCategoryName());
     }
 
+    /*
     public List<Category> getCategoryList() {
         List<Category> categories = categoryRepository.findAll();
         return categories;
@@ -41,8 +71,21 @@ public class CategoryService {
         return categoryRepository.findById(id).get();
     }
 
+    public void updateCategory(Long id, UpdateCategoryDto dto){
+        // 조회
+        Optional<Category> category = categoryRepository.findById(id);
+        if (category.isEmpty()){
+            throw new RuntimeException("존재하지 않는 카테고리 조회");
+        }
+        Category use = category.get();
+
+        use.update(dto);
+
+        categoryRepository.save(use);
+    }
+
     public void deleteCategory(Long id){
-        categoryRepository.deleteById(id);
+        categoryRepository.deleteById(id); */
     }
 
 
