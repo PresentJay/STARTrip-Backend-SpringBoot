5d8d22bb59005ff2ded679e795fca8820dc71be7
kgyury <mandarining0918@gmail.com>
2022-03-22 22:09:08 +0900

    feat: Modified CategoryDomain

--- startrip-codebase/src/main/java/com/startrip/codebase/domain/category/Category.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/domain/category/Category.java
@@ -17,27 +17,16 @@ public class Category {
     @Id
     @Column(name = "category_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long categoryId; // PK
+    private Long id; // PK
 
-    @Column(name = "category_parent_id")
-    private Long categoryParentID;
+    @ManyToOne(fetch = FetchType.LAZY )
+    @JoinColumn(name = "category_id")
+    private Category categoryParent;
 
-    @Column(name = "category_name")
+    @Column(name = "category_name", nullable = false)
     private String categoryName;
-
-    //자식 카테고리들
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoryParentID", cascade = CascadeType.PERSIST)
-    @Column (name ="category_child_id")
-    private List<Category> categoryChildId = new ArrayList<>();
 
     @Column(nullable = false)
     private Integer depth;
-
-    public void update(CreateCategoryDto dto){
-        this.categoryParentID = dto.getCategoryParentId();
-        this.categoryId = dto.getCategoryId();
-        this.categoryName = dto.getCategoryName();
-        this.depth = dto.getDepth();
-    }
 
 }
