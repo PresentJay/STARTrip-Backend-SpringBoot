b8acb11c759305ba30cee8f284a16b0bb57cfcc8
kgyury <mandarining0918@gmail.com>
2022-04-02 18:27:14 +0900

    feat: Implement CreateAPI for op_time

--- startrip-codebase/src/main/java/com/startrip/codebase/controller/OperatingTimeController.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/controller/OperatingTimeController.java
@@ -1,6 +1,7 @@
 package com.startrip.codebase.controller;
 
 import com.startrip.codebase.dto.operatingTime.ResponseOpTimeDto;
+import com.startrip.codebase.service.OperatingTimeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +15,17 @@ import java.sql.Timestamp;
 @RequestMapping("/api/place")
 public class OperatingTimeController {
 
-    // private final OperatingTimeService operatingTimeService;
+     private final OperatingTimeService operatingTimeService;
+
+
+     public OperatingTimeController(OperatingTimeService operatingTimeService ) {
+         this.operatingTimeService = operatingTimeService;
+     }
 
     // CREATE
     @PostMapping("/optime")
     public ResponseEntity createOpTime(@RequestParam Long placeId, ResponseOpTimeDto dto){
+         operatingTimeService.createOpTime(placeId, dto);
             // service
             return  new ResponseEntity<>("운영시간 생성",HttpStatus.OK);
     }
@@ -53,9 +60,8 @@ public class OperatingTimeController {
 
     // DELETE opTime
     @DeleteMapping("/optime/{placeId}/{optimeId}")
-    public void deleteOpTime(@PathVariable("placeId") Long placeId, @PathVariable("optimeId") Long optimeId{
+    public void deleteOpTime(@PathVariable("placeId") Long placeId, @PathVariable("optimeId") Long optimeId){
 
     }
-
 
 }
---
+++ startrip-codebase/src/main/java/com/startrip/codebase/service/OperatingTimeService.java
@@ -0,0 +1,37 @@
+package com.startrip.codebase.service;
+
+import com.startrip.codebase.domain.Operating_time.OperatingTime;
+import com.startrip.codebase.domain.Operating_time.OperatingTimeRepository;
+import com.startrip.codebase.domain.category.CategoryRepository;
+import com.startrip.codebase.domain.notice.Notice;
+import com.startrip.codebase.domain.place.Place;
+import com.startrip.codebase.domain.place.PlaceRepository;
+import com.startrip.codebase.dto.operatingTime.ResponseOpTimeDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OperatingTimeService {
+
+
+    private final OperatingTimeRepository operatingTimeRepository;
+    private final PlaceRepository placeRepository;
+
+
+    @Autowired
+    public OperatingTimeService(OperatingTimeRepository operatingTimeRepository, PlaceRepository placeRepository) {
+        this.operatingTimeRepository = operatingTimeRepository;
+        this.placeRepository = placeRepository;
+    }
+
+    public void createOpTime (Long placeId , ResponseOpTimeDto dto) {
+        //해당 placeId가 존재하는지 확인한 후,
+        placeRepository.findById(placeId)
+                .orElseThrow(() -> new RuntimeException("해당 장소가 존재하지 않습니다."));
+
+        // 존재하는 경우 해당 optime을 dto내용에 맞추어 생성시키자.
+        OperatingTime operatingTime = OperatingTime.of(placeId, dto);
+
+        operatingTimeRepository.save(operatingTime);
+    }
+}
