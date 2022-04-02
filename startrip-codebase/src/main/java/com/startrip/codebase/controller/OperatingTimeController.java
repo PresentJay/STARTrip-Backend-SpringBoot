bc7126169837ccd51a11d7cb90ed6018e92d722e
kgyury <mandarining0918@gmail.com>
2022-04-02 17:52:08 +0900

    chore: Modify  opTime Controller parameter (Pathvariable)

---
+++ startrip-codebase/src/main/java/com/startrip/codebase/controller/OperatingTimeController.java
@@ -0,0 +1,61 @@
+package com.startrip.codebase.controller;
+
+import com.startrip.codebase.dto.operatingTime.ResponseOpTimeDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/api/place")
+public class OperatingTimeController {
+
+    // private final OperatingTimeService operatingTimeService;
+
+    // CREATE
+    @PostMapping("/optime")
+    public ResponseEntity createOpTime(@RequestParam Long placeId, ResponseOpTimeDto dto){
+            // service
+            return  new ResponseEntity<>("운영시간 생성",HttpStatus.OK);
+    }
+
+    // GET All
+    @GetMapping ("/optime/{placeId}")
+    public void getOpTimeAll(@PathVariable("placeId") Long placeId){
+    }
+
+    // GET All in specific place
+    @GetMapping ("/optime/{placeId}")
+    public void geOpTimeAll_inSpecificPlace(@PathVariable("placeId") Long placeId){
+    }
+
+    // GET All in specific place in specific timestamp
+    @GetMapping ("/optime/{placeId}/{timestamp}")
+    public void getOpTime_inCurrentTimestamp(@PathVariable("placeId") Long placeId, @PathVariable Timestamp timestamp){
+        // TODO: 타임스탬프 형태로 파라미터가 들어올 수 있는 건지, 변혀앻야하는 건지 확인해보자
+
+
+    }
+
+    // Get specific opTime
+    @GetMapping ("/optime/{placeId}/{optimeId}")
+    public void getOptime_only(@PathVariable("placeId") Long placeId, @PathVariable("optimeId") Long optimeId){
+    }
+
+    // UPDATE opTime
+    @PutMapping ("/optime/{placeId}/{optimeId}")
+    public void updateOpTime(@PathVariable("placeId") Long placeId, @PathVariable("optimeId") Long optimeId){
+    }
+
+    // DELETE opTime
+    @DeleteMapping("/optime/{placeId}/{optimeId}")
+    public void deleteOpTime(@PathVariable("placeId") Long placeId, @PathVariable("optimeId") Long optimeId{
+
+    }
+
+
+}
