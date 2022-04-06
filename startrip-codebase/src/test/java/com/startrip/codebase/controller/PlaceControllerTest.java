bbc2e67e86fb5a17cce993dbd27e390c4085650e
Younggun Park <ygpark0922@gmail.com>
2022-04-06 14:14:28 +0900

    feat: Place Test create

--- startrip-codebase/src/test/java/com/startrip/codebase/controller/PlaceControllerTest.java
+++ startrip-codebase/src/test/java/com/startrip/codebase/controller/PlaceControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class PlaceControllerTest {
 
     public MockMvc mockMvc;
