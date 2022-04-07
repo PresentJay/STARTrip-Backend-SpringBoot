9ab4c83da8ccfb5342bbd01bf16a081820e98684
Hoon9901 <shonn.dev@gmail.com>
2022-04-07 11:33:54 +0900

    test: 회원가입 테스트 추가

--- startrip-codebase/src/test/java/com/startrip/codebase/controller/UserControllerTest.java
+++ startrip-codebase/src/test/java/com/startrip/codebase/controller/UserControllerTest.java
@@ -1,20 +1,63 @@
 package com.startrip.codebase.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.startrip.codebase.constant.Role;
 import com.startrip.codebase.domain.user.User;
+import com.startrip.codebase.dto.SignUpDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ActiveProfiles("test")
 class UserControllerTest {
 
     @Autowired
-    UserController userController;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserController userController;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @DisplayName("회원가입 API가 작동한다")
+    @Test
+    void test1() throws Exception {
+        SignUpDto dto = new SignUpDto();
+        dto.setEmail("test@test.com");
+        dto.setPassword("1234");
+        dto.setName("name");
+        dto.setNickname("nickname");
+
+        mockMvc.perform(
+                        post("/api/user/signup")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(dto))
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 
 }
