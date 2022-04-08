fcaa91a1b693630d7bd3840b77001517c25ec573
장희재 <becca4011@naver.com>
2022-04-08 17:08:54 +0900

    fix: User 삭제 수정

--- startrip-codebase/src/main/java/com/startrip/codebase/domain/place_trip/PlaceTrip.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/domain/place_trip/PlaceTrip.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -23,6 +25,7 @@ public class PlaceTrip {
 
     @OneToOne
     @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User userId;
 
     @Column(name = "user_partner")
