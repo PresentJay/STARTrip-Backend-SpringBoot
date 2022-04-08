cb87447f8e86a709aa8b36fad818bc6185a94c0a
Hoon9901 <shonn.dev@gmail.com>
2022-04-08 12:09:36 +0900

    test: 로그인/회원가입, JWT 토큰 테스트 코드 작성

--- startrip-codebase/src/test/java/com/startrip/codebase/controller/UserControllerTest.java
+++ startrip-codebase/src/test/java/com/startrip/codebase/controller/UserControllerTest.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.startrip.codebase.constant.Role;
 import com.startrip.codebase.domain.user.User;
+import com.startrip.codebase.dto.LoginDto;
 import com.startrip.codebase.dto.SignUpDto;
+import com.startrip.codebase.jwt.TokenProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -13,10 +15,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.mapping.TextScore;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.IOException;
 
@@ -36,11 +41,12 @@ class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
-    private UserController userController;
+    private TokenProvider tokenProvider;
 
     private ObjectMapper objectMapper = new ObjectMapper();
+
+    private String jwtToken = "";
 
     @DisplayName("회원가입 API가 작동한다")
     @Test
@@ -57,7 +63,47 @@ class UserControllerTest {
                                 .content(objectMapper.writeValueAsString(dto))
                 )
                 .andExpect(status().isOk())
+                .andExpect(content().string(dto.getEmail() + " 가입완료"))
                 .andDo(print());
+
+    }
+
+    @DisplayName("로그인 API가 작동한다")
+    @Test
+    void test2() throws Exception {
+
+        LoginDto dto = new LoginDto();
+        dto.setEmail("test@test.com");
+        dto.setPassword("1234");
+
+        mockMvc.perform(
+                        post("/api/user/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(dto))
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @DisplayName("로그인 후 JWT 토큰이 발급되고, 해당 토큰을 통해 인증 객체를 추출할 수 있다")
+    @Test
+    void test3() throws Exception {
+        LoginDto dto = new LoginDto();
+        dto.setEmail("test@test.com");
+        dto.setPassword("1234");
+
+        MvcResult mvcResult = mockMvc.perform(
+                        post("/api/user/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(dto))
+                )
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+        jwtToken = mvcResult.getResponse().getContentAsString();
+
+        Authentication authentication = tokenProvider.getAuthentication(jwtToken);
+        assertThat(authentication.getName()).isEqualTo(dto.getEmail());
     }
 
 }
