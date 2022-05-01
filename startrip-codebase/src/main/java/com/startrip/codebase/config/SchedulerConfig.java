edb629afed741ac7c268a4a8891904a53d88ef3f
kgyury <mandarining0918@gmail.com>
2022-05-01 22:01:47 +0900

    feat: setting SpringScheduler

---
+++ startrip-codebase/src/main/java/com/startrip/codebase/config/SchedulerConfig.java
@@ -0,0 +1,13 @@
+package com.startrip.codebase.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+    public static void main (String [] args) {
+    }
+}
