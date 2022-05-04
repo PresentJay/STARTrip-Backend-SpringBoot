3734f71d2930dab71e9be1b55ab0a43f439b211c
Hoon9901 <shonn.dev@gmail.com>
2022-05-04 19:44:36 +0900

    fix: NoticeComment TEST 환경 프로파일 적용

--- startrip-codebase/src/test/java/com/startrip/codebase/controller/NoticeCommentControllerTest.java
+++ startrip-codebase/src/test/java/com/startrip/codebase/controller/NoticeCommentControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -42,6 +43,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class NoticeCommentControllerTest {
     @Autowired
