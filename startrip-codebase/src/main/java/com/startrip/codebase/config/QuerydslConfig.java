f2ffb668f1a8bdb8583f82035d1c87111c15b376
Hoon9901 <shonn.dev@gmail.com>
2022-05-24 18:26:44 +0900

    fix: QueryDSL, 어노테이션 수정

--- startrip-codebase/src/main/java/com/startrip/codebase/config/QuerydslConfig.java
+++ startrip-codebase/src/main/java/com/startrip/codebase/config/QuerydslConfig.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 public class QuerydslConfig {
-
-    @Autowired
+    @PersistenceContext
     private EntityManager entityManager; // Null
 
     @Bean
